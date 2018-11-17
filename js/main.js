eae8fcf2a86c43cfed20cb2a9e88f79175427498
Irina Liang <mysmile15@gmail.com>
2018-11-17 12:11:14 -0800

    mac

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-  $('.main').onepage_scroll({
-    sectionContainer: 'section',
-    responsiveFallback: 600,
-    loop: true
-  });
+$(window).scroll(function() {
+    $('#animatedElement').each(function() {
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 400) {
+            $(this).addClass("fadeInUp");
+        }
+    });
 });
